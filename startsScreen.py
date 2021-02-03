f2928667df25332b878178092fdd98c9707fea11
Udbhav Devarakonda <udbhav.devarakonda@gmail.com>
2021-02-03 12:06:56 -0500

    not much new in this commit

--- startsScreen.py
+++ startsScreen.py
@@ -8,11 +8,16 @@ class Application (Frame):
 
     def create_widgets(self):
         
-        self.mapbutton1 = Button(self, text = "American States", fg = "blue", font = "Times 20 bold", command = self.destroy_button)
+        Label(self, text = "").grid(row = 0, column = 0)
+        Label(self, text = "").grid(row = 1, column = 1)
+
+        self.mapbutton1 = Button(self, text = "American States", fg = "blue", font = "Times 20 bold", command = self.destroy_mapbutton1)
         self.mapbutton1.grid(row = 2, column = 2)
 
-    def destroy_button(self):
+
+    def destroy_mapbutton1(self):
         self.mapbutton1.destroy()
+
 
 
 
