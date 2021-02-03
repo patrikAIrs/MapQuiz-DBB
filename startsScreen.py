f4a24947101ea08380a133ada716de613ab95063
Udbhav Devarakonda <udbhav.devarakonda@gmail.com>
2021-02-03 11:58:20 -0500

    Added mapbutton1

--- startsScreen.py
+++ startsScreen.py
@@ -1,2 +1,28 @@
-import tkinter from *
+from tkinter import *
+
+class Application (Frame):
+    def __init__ (self, master):
+        super(Application, self).__init__(master)
+        self.grid()
+        self.create_widgets()
+
+    def create_widgets(self):
+        
+        self.mapbutton1 = Button(self, text = "American States", fg = "blue", font = "Times 20 bold", command = self.destroy_button)
+        self.mapbutton1.grid(row = 2, column = 2)
+
+    def destroy_button(self):
+        self.mapbutton1.destroy()
+
+
+
+root = Tk()
+root.title("Map Quiz")
+root.geometry("400x200")
+
+app = Application(root)
+
+root.mainloop()
+
+
 
