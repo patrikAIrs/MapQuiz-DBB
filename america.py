881282d3a02a7380c76d89dab18ade5433e7e1b1
patrikAIrs <patrickdb123@gmail.com>
2021-02-04 12:16:49 -0500

    started work on canvas widget

--- america.py
+++ america.py
@@ -6,13 +6,17 @@ class Application (Frame):
         self.grid()
         self.create_widgets()
 
+
     def create_widgets(self):
+        acanvas = Canvas(parent, width = 500, height = 400, background = 'gray')
 
 
-        muricamap = PhotoImage(file = "AMERICANSTATES2.png")
-        w = Label(self, image = muricamap)
-        w.photo = muricamap
-        w.grid(row = 0, column = 0, sticky = NSEW)
+
+        # muricamap = PhotoImage(file = "AMERICANSTATES2.png")
+        # w = Label(self, image = muricamap)
+        # w.photo = muricamap
+        # w.grid(row = 0, column = 0, sticky = NSEW)
+        # https://tkdocs.com/tutorial/canvas.html
 
 
 
