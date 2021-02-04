e3626f9eee76ec3fb91e17b535d48989dc4bdb16
patrikAIrs <patrickdb123@gmail.com>
2021-02-04 11:46:01 -0500

    push

--- america.py
+++ america.py
@@ -10,7 +10,11 @@ class Application (Frame):
         
         Label(self, text = "").grid(row = 0, column = 0)
         Label(self, text = "").grid(row = 1, column = 1)
-        
+
+        muricamap = PhotoImage(file = "AMERICANSTATES2")
+        w = Label(self, image = muricamap)
+        w.photo = muricamap
+        w.grid(row = 4, column = 0, sticky = EW)
 
 
 
