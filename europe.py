db5aa198d638e59c2b0c9506354226483618128c
Udbhav Devarakonda <udbhav.devarakonda@gmail.com>
2021-02-08 12:00:05 -0500

    changed europe

--- europe.py
+++ europe.py
@@ -1,14 +1,36 @@
 from tkinter import *
+#from tkinter import ttk
 
-class EuropeSketchpad(Canvas):
-    def __init__(self, parent, **kwargs):
-        super().__init__(parent, **kwargs)
-        self.bind("<Button-1>", self.save_posn)
-        self.bind("<B1-Motion>", self.add_line)
-        
-    def save_posn(self, event):
-        self.lastx, self.lasty = event.x, event.y
+class Application (Frame):
+    def __init__ (self, master):
+        super(Application, self).__init__(master)
+        self.grid()
+        self.create_widgets()
 
-    def add_line(self, event):
-        self.create_line((self.lastx, self.lasty, event.x, event.y))
-        self.save_posn(event)
+
+    def create_widgets(self):
+        acanvas = Canvas(self, width = 500, height = 400, background = 'gray')
+        statename = Entry(acanvas)
+        acanvas.create_image(10,10, anchor ='nw')
+
+
+
+        # muricamap = PhotoImage(file = "AMERICANSTATES2.png")
+        # w = Label(self, image = muricamap)
+        # w.photo = muricamap
+        # w.grid(row = 0, column = 0, sticky = NSEW)
+        # https://tkdocs.com/tutorial/canvas.html
+
+
+
+
+
+
+
+root = Tk()
+root.title("America")
+root.geometry("1830x1080")
+
+app = Application(root)
+
+root.mainloop()
