c9cdafb95041f755c0f6633446e9e1f3a96e5427
Udbhav Devarakonda <udbhav.devarakonda@gmail.com>
2021-02-08 15:40:55 -0500

    more changes

--- europe.py
+++ europe.py
@@ -8,16 +8,19 @@ class EuropeSketchpad (Frame):
 
 
     def create_widgets(self):
-        acanvas = Canvas(self, width = 1830, height = 1080, background = 'gray')
-        acanvas.pack()
+        # acanvas = Canvas(self, width = 1830, height = 1080, background = 'red')
+        # acanvas.pack()
 
-        map = PhotoImage(file = "AMERICANSTATES2.png")
-        acanvas.create_image(10,10, image = map, anchor = NW)
+        # acanvas.create_image(10,10, image = map, anchor = NW)
 
-        statename = Entry(acanvas)
-        acanvas.create_window(1000,500, window =statename)
+        # statename = Entry(acanvas)
+        # acanvas.create_window(1000,500, window =statename)
 
 
+        europemap = PhotoImage(file = "EUROPEMAP.png")
+        europe_map_image = Label(self, image = europemap)
+        europe_map_image.photo = europemap
+        europe_map_image.grid(row = 0, column = 0)
 
         # muricamap = PhotoImage(file = "AMERICANSTATES2.png")
         # w = Label(self, image = muricamap)
