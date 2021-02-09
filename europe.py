75b88dab15d371500a4d651c8fb66cd680dd86c5
Udbhav Devarakonda <udbhav.devarakonda@gmail.com>
2021-02-09 10:53:03 -0500

    made image and canvas work

--- europe.py
+++ europe.py
@@ -8,17 +8,16 @@ class EuropeSketchpad (Frame):
 
 
     def create_widgets(self):
-        # acanvas = Canvas(self, width = 1830, height = 1080, background = 'red')
-        # acanvas.pack()
 
-        # acanvas.create_image(10,10, image = map, anchor = NW)
 
-        # statename = Entry(acanvas)
-        # acanvas.create_window(1000,500, window =statename)
+        ecanvas = Canvas(self, width = 1280, height = 1024)
+        ecanvas.pack(expand = YES, fill = BOTH)
 
+        countryname = Entry(ecanvas)
+        ecanvas.create_window(10, 10, window = countryname)
 
         europemap = PhotoImage(file = "EUROPEMAP3.png")
-        europe_map_image = Label(self, image = europemap)
+        europe_map_image = Label(ecanvas, image = europemap)
         europe_map_image.photo = europemap
         europe_map_image.grid(row = 0, column = 0)
 
