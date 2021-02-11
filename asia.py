ba7907e15ca6f27e12771a4b1f03f1db8614f067
Udbhav Devarakonda <udbhav.devarakonda@gmail.com>
2021-02-10 20:03:00 -0500

    Made all countries work

--- asia.py
+++ asia.py
@@ -18,10 +18,12 @@ class AsiaScreen (Frame):
         asmap.photo = asiamap
         asmap.grid(row = 0, column = 0)
 
+        self.number_of_tries = 0
 
-        self.country_list = ["New York", "Oregon", "California", "Montana", "Missouri", "Texas"]
-        self.x_coordinates = [1580, 215, 160, 565, 1040, 820]
-        self.y_coordinates = [300, 230, 600, 160, 550, 840]
+
+        self.country_list = ["China", "India", "Mongolia", "Kazakhstan", "S. Arabia", "Iran"]
+        self.x_coordinates = [600, 450, 563, 320, 170, 240]
+        self.y_coordinates = [360, 480, 265, 257, 460, 400]
         self.country_indexes = [0, 1, 2, 3, 4, 5]
 
         self.current_country_index = random.choice(self.country_indexes)
@@ -33,6 +35,10 @@ class AsiaScreen (Frame):
         self.right_wrong = Label(self, text = "", font = "Times 20 bold")
         self.right_wrong.config(width = 10, height = 3)
         ascanvas.create_window(100, 200, window = self.right_wrong)
+
+        self.try_number = Label(self, text = "%s: %d" % ("Tries", self.number_of_tries), font = "Times 20 bold")
+        self.try_number.config(width = 10, height = 3)
+        ascanvas.create_window(300, 100, window = self.try_number)
 
 
 
@@ -50,9 +56,15 @@ class AsiaScreen (Frame):
         selected_index = int(self.country_radiobutton_value.get())
         if selected_index != self.current_country_index:
             self.right_wrong.config(text = "Incorrect")
+            self.number_of_tries += 1
+            self.try_number.config(text = "%s: %d" % ("Tries", self.number_of_tries))
+
         else:
             self.right_wrong.config(text = "Correct!")
             self.change_country_text()
+            self.number_of_tries += 1
+            self.try_number.config(text = "%s: %d" % ("Tries", self.number_of_tries))
+
 
     def change_country_text(self):
         if len(self.country_indexes) > 0:
@@ -62,8 +74,9 @@ class AsiaScreen (Frame):
             self.current_country_index = random.choice(self.country_indexes)
             self.country_text.config(text = self.country_list[self.current_country_index])
         else:
-           print("You did it!")
-           self.quit()
+            self.number_of_tries += 1
+            print("You found all countries in %d guesses" % (self.number_of_tries))
+            self.quit()
         
 
 
